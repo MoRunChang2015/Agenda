875caf70bea5355f8983850b15d0e26b4818b06c
tommyleo <137581768@qq.com>
2016-08-12 15:37:30 +0800

    modify files structure

--- gtest/src/utility.h
+++ gtest/src/utility.h
@@ -24,12 +24,12 @@ namespace utility {
     void recFiles();
 
     //  Path to csv
-    const char *const userPath = "tmp/data/users.csv";
-    const char *const meetingPath = "tmp/data/meetings.csv";
-    const char *const stdUserPath = "tmp/standardData/rec_users.csv";
-    const char *const stdMeetingPath = "tmp/standardData/rec_meetings.csv";
-    const char *const cmpUserPath = "tmp/standardData/users.csv";
-    const char *const cmpMeetingPath = "tmp/standardData/meetings.csv";
+    const char *const userPath = "tmp/users.csv";
+    const char *const meetingPath = "tmp/meetings.csv";
+    const char *const stdUserPath = "tmp/rec_users.csv";
+    const char *const stdMeetingPath = "tmp/rec_meetings.csv";
+    const char *const cmpUserPath = "tmp/cmpUsers.csv";
+    const char *const cmpMeetingPath = "tmp/cmpMeetings.csv";
 
 }
 
